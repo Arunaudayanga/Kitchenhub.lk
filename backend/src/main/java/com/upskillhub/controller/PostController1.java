5ac3edf1ea69402ee1a59c34e72f35c3df39a14a
HimashaNadeen <87822331+HimashaNadeen@users.noreply.github.com>
2025-05-10 23:45:06 +0530

    Update PostController1.java

--- backend/src/main/java/com/upskillhub/controller/PostController1.java
+++ backend/src/main/java/com/upskillhub/controller/PostController1.java
@@ -82,7 +82,13 @@ public class PostController {
         return ResponseEntity.ok(postService.addComment(postId, userPrincipal.getId(), commentRequest.getContent()));
     }
     
-   
+   @DeleteMapping("/{postId}/comments/{commentId}")
+    public ResponseEntity<Post> deleteComment(
+            
+            @PathVariable String commentId,
+            @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        return ResponseEntity.ok(postService.removeComment(postId, commentId, userPrincipal.getId()));
+
      @GetMapping("/user/{userId}")
     public ResponseEntity<Page<Post>> getUserPosts(
             @PathVariable String userId,
